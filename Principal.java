8ab0cf7daec41dbbd52f694879bc06f82a0a4bbe
luis-guilhermee <luis.marianosenai@gmail.com>
2026-02-18 12:08:17 -0300

    primeiro commit

---
+++ Principal.java
@@ -0,0 +1,11 @@
+package aulaJPA.aula1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Esse código printa o texto na tela
+		System.out.println("Luisaoo cara de sabão, e Lucão namorado do Millenão");
+	}
+
+}
---
+++ Somador.java
@@ -0,0 +1,27 @@
+package aulaJPA.aula1;
+
+import java.util.Scanner;
+
+public class Somador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Variáveis
+		double a,b,som;
+		//Instaciar classe Scanner
+		Scanner ler = new Scanner (System.in) ;
+		
+		//Etrada de dados
+		System.out.println("Informe valor 1: ");
+		a = ler.nextDouble() ;
+		
+		System.out.println("Informe valor 2: ");
+		b = ler.nextDouble() ;
+		
+		//Processamento
+		som = a+b;
+		//Saída
+		System.out.println("O resultado é: " + som);
+	}
+
+}
